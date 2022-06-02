740cc7d0a46d18dfc231f16fe2c4abb6e898d03f
narditek12 <narditek1@gmail.com>
2022-06-02 04:43:22 -0700

    nwe ideao

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for zero_to_nine in range(00, 99):
-    print("{:02d}".format(zero_to_nine), end=', ')
-print(99)
+for i in range(100):
+    print("{:0>2}".format(i), end=", " if i < 99 else "\n")
+
