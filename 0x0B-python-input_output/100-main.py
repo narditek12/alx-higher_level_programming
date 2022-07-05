2a58570387d0313854a853233ffbee78b0635868
narditek12 <51337463+narditek12@users.noreply.github.com>
2022-07-05 11:40:17 -0700

    Create 100-main.py

---
+++ 0x0B-python-input_output/100-main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+append_after = __import__('100-append_after').append_after
+
+append_after("append_after_100.txt", "Python", "\"C is fun!\"\n")
+
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
